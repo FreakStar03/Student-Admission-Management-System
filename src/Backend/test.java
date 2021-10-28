f74ed047a6109e188afcc18432d51842daccda06
freakstar03 <freakstar03@gmail.com>
2021-10-28 20:25:07 +0530

    admin panel

--- src/Backend/test.java
+++ src/Backend/test.java
@@ -5,48 +5,101 @@ import java.sql.*;
 //import java.util.Arrays;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
+ import javax.swing.table.DefaultTableModel;
+// import javax.tools.JavaFileObject;
+//import javax.swing.table.TableColumnModel;
 
-public class test extends JPanel{  
-   public test(){  
-      try{
-         Class.forName("com.mysql.cj.jdbc.Driver");  
-         Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/student","Chiragsp","admin");  
-         PreparedStatement Pstatement=con.prepareStatement("SELECT firstname, middlename, lastname FROM PersonalData WHERE id = ?");
-         Pstatement.setInt(1, 8);
-         ResultSet rs = Pstatement.executeQuery();
-         String[] columnNames = {"Student ID", "Name", "DOB"};
-         DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
-         
-         while (rs.next()) {
-             String title = rs.getString("firstname");
-             String season = rs.getString("middlename");
-             String episode = rs.getString("lastname");
-         
-             // create a single array of one row's worth of data
-             String[] data = { title, season, episode } ;
-         
-             // and add this row of data into the table model
-             tableModel.addRow(data);
-         }
+// import com.mysql.cj.xdevapi.JsonArray;
+
+public class test extends JFrame{ 
+   static int Tstart = 0; 
+   static int Tend = 10; 
+   public test(){ 
+         setTitle("Test");
+         setSize(500,500);
+
+         JPanel panel = new JPanel();
+         String[] columnNames = { "PdID", "id", "firstname", "middlename", "lastname", "phoneno1", "phoneno2",
+               "address", "email", "dob", "city", "state", "pincode", "photoAddress", "IdAddress" };
+         DefaultTableModel tableModel = new DefaultTableModel(my_db_select(), columnNames);
+
          JTable jt = new JTable(tableModel); // to create a new JTable
-
-         jt.setPreferredScrollableViewportSize(new Dimension(450, 63));
+         jt.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+         jt.setPreferredScrollableViewportSize(new Dimension(450, 300));
          jt.setFillsViewportHeight(true);
+         jt.setDefaultEditor(Object.class, null);
 
          JScrollPane jps = new JScrollPane(jt);
-         add(jps);
-      }catch(Exception e){ System.out.println(e);System.out.println("excpt");}        
-   }
+         JButton prevbtn = new JButton("Prev");
+         JButton nextbtn = new JButton("Next");
+
+         panel.add(jps);
+         panel.add(prevbtn);
+         panel.add(nextbtn);
+
+         prevbtn.addActionListener(e -> {
+
+            if (Tstart == 0 && Tend == 10) {
+               Tstart = 0;
+               Tend = 10;
+            } else {
+               Tstart -= 10;
+               Tend -= 10;
+               test main = new test();
+               main.setVisible(true);
+               // setVisible(false);
+               dispose();
+            }
+         });
+         nextbtn.addActionListener(e -> {
+            Tstart += 10;
+            Tend += 10;
+            test main = new test();
+            main.setVisible(true);
+            // setVisible(false);
+            dispose();
+
+         });
+
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         add(panel);
+         setVisible(true);
+      }
+
    public static void main(String[] args) {
-      JFrame jf = new JFrame();
-      test t = new test();
-      jf.setTitle("Test");
-      jf.setSize(500,500);
-      jf.setVisible(true);
-      jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      jf.add(t);  
-}}
+      new test();
+}
+
+   public  String[][] my_db_select() {
+      ////////////
+      String[][] data ={ }  ;
+      int start = Tstart;
+      int end = Tend;
+      try{  
+         Class.forName("com.mysql.cj.jdbc.Driver");  
+         Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/student","Chiragsp","admin");  
+         Statement st=con.createStatement();
+         ResultSet rs=st.executeQuery("SELECT * FROM PersonalData LIMIT " + start + "," + end);
+         ResultSetMetaData rsmd = rs.getMetaData();
+         String[][] dataTemp = new String[10][rsmd.getColumnCount()]; // [rows][columns]
+         data = dataTemp;  
+      
+      // Looping to store result in returning array data // 
+         int i=0;
+         while(rs.next())  {
+         for(int j=0;j<rsmd.getColumnCount();j++) {
+         System.out.print(rs.getString(j+1));
+         data[i][j]=rs.getString(j+1);
+         }
+         //System.out.println();
+         i=i+1;
+         }
+      con.close();  
+   }catch(Exception e){ System.out.println(e);} 
+
+   return data;
+      }
+   }
 // SELECT a.*, b.*
 // FROM studentReg as a
 // INNER JOIN PersonalData as b ON a.id=b.id
--- src/Frames/AdminPortal.java
+++ src/Frames/AdminPortal.java
@@ -3,7 +3,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Dimension;
+import java.sql.*;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+ import javax.swing.table.DefaultTableModel;
 public class AdminPortal extends JFrame{
+    static int Tstart = 0; 
+    static int Tend = 20; 
+
     public AdminPortal(){
         setTitle("Admin Portal");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -48,7 +56,7 @@ public class AdminPortal extends JFrame{
         pane.add(BranchField);
         pane.add(SearchField);
         pane.add(SBField);
-        pane.add(bg);
+
         //style
         title.setFont(new Font("Serif", Font.PLAIN, 30));
         title.setOpaque(true);
@@ -69,17 +77,57 @@ public class AdminPortal extends JFrame{
         SBField.setBounds(1089,111,148,35);
         bg.setBounds(19,170,1241,520);
 
+         String[] columnNames = { "PdID", "id", "firstname", "middlename", "lastname", "phoneno1", "phoneno2",
+               "address", "email", "dob", "city", "state", "pincode", "photoAddress", "IdAddress" };
+         DefaultTableModel tableModel = new DefaultTableModel(my_db_select(), columnNames);
 
-        setVisible(true);
 
-        //Logic
-        // subbtn.addActionListener(e ->
-        // {
-        //     StudentPortal rg = new StudentPortal();
-        //     setVisible(false);
-        //     rg.setVisible(true);
-        // });
+         JPanel tablePanel = new JPanel();
+         JTable jt = new JTable(tableModel); // to create a new JTable
+         jt.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+         //jt.setBounds(58,202,1165,460);
+         jt.setPreferredScrollableViewportSize(new Dimension(800, 320 ));
+         jt.setFillsViewportHeight(true);
+         //jt.setDefaultEditor(Object.class, null);
 
+         JScrollPane jps = new JScrollPane(jt);
+         JButton prevbtn = new JButton("Prev");
+         JButton nextbtn = new JButton("Next");
+         tablePanel.setBounds(58,202,1165,457);
+         tablePanel.add(jps);
+        //  tablePanel.add(prevbtn);
+        //  tablePanel.add(nextbtn);
+         prevbtn.setBounds(489,590,137,44);
+         nextbtn.setBounds(710,590,137,44);
+         pane.add(prevbtn);
+         pane.add(nextbtn);
+         pane.add(tablePanel);
+         pane.add(bg);
+         //setBounds(54,202,1165,460);
+
+         prevbtn.addActionListener(e -> {
+            if (Tstart == 0 && Tend == 20) {
+               Tstart = 0;
+               Tend = 20;
+               //prevbtn.setEnabled(false);
+            } else {
+               Tstart -= 20;
+               Tend -= 20;
+               AdminPortal main = new AdminPortal();
+               main.setVisible(true);
+               // setVisible(false);
+               dispose();
+            }
+         });
+         nextbtn.addActionListener(e -> {
+            Tstart += 20;
+            Tend += 20;
+            AdminPortal main = new AdminPortal();
+            main.setVisible(true);
+            // setVisible(false);
+            dispose();
+
+         });
         Logout.addMouseListener(new MouseAdapter()   {   
             public void mouseClicked(MouseEvent e)   
             { 
@@ -88,18 +136,37 @@ public class AdminPortal extends JFrame{
                 rg.setVisible(true);   
             }});
 
-        //ALL STYLING
-        // Component[] component = pane.getComponents();
-        // for(int i=0; i<component.length; i++)
-        // {
-        //     if (component[i] instanceof JButton)
-        //     {
-        //         JButton button = (JButton)component[i];
-        //         button.setFocusPainted(false);
-        //     }
-        // };
-
+        setVisible(true);
     }
+    public  String[][] my_db_select() {
+        ////////////
+        String[][] data ={ }  ;
+        int start = Tstart;
+        int end = Tend;
+        try{  
+           Class.forName("com.mysql.cj.jdbc.Driver");  
+           Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/student","Chiragsp","admin");  
+           Statement st=con.createStatement();
+           ResultSet rs=st.executeQuery("SELECT * FROM PersonalData LIMIT " + start + "," + end);
+           ResultSetMetaData rsmd = rs.getMetaData();
+           String[][] dataTemp = new String[20][rsmd.getColumnCount()]; // [rows][columns]
+           data = dataTemp;  
+        
+        // Looping to store result in returning array data // 
+           int i=0;
+           while(rs.next())  {
+           for(int j=0;j<rsmd.getColumnCount();j++) {
+           System.out.print(rs.getString(j+1));
+           data[i][j]=rs.getString(j+1);
+           }
+           //System.out.println();
+           i=i+1;
+           }
+        con.close();  
+     }catch(Exception e){ System.out.println(e);} 
+  
+     return data;
+        }
 }
 
 
